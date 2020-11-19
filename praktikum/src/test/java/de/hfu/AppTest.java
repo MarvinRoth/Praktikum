fdb0387e965166d432b0e2e2c0d9ca7da7e479ae
Marvin <marv070@hotmail.de>
2020-11-19 15:38:00 +0100

    Aufgaben Blatt 6 Aufgabe1-6

--- praktikum/src/test/java/de/hfu/AppTest.java
+++ praktikum/src/test/java/de/hfu/AppTest.java
@@ -1,20 +1,51 @@
 package de.hfu;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testUtil() {
+        assertTrue(Util.istErstesHalbjahr(1));
+        assertTrue(Util.istErstesHalbjahr(2));
+        assertTrue(Util.istErstesHalbjahr(3));
+        assertTrue(Util.istErstesHalbjahr(4));
+        assertTrue(Util.istErstesHalbjahr(6));
+        }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUtilExcept(){
+        Util.istErstesHalbjahr(0);
+        Util.istErstesHalbjahr(13);
+        assertTrue(Util.istErstesHalbjahr(12));
+        assertTrue(Util.istErstesHalbjahr(7));
+    }
+
+    @Test
+    public void testQ(){
+        Queue test = new Queue(3);
+        test.enqueue(5);
+        test.enqueue(6);
+        test.enqueue(9);
+        assertEquals(5, test.dequeue());
+        assertEquals(6, test.dequeue());
+        assertEquals(9, test.dequeue());
+        try{
+            test.dequeue();
+            fail();
+        }
+        catch(IllegalStateException e){
+
+        }
+        test.enqueue(5);
+        assertEquals(5, test.dequeue());
+        for (int i=0; i < 1000; ++i){
+            test.enqueue(i);
+        }
+
+        assertEquals(0, test.dequeue());
+        assertEquals(1, test.dequeue());
+        assertEquals(999, test.dequeue());
     }
 }
